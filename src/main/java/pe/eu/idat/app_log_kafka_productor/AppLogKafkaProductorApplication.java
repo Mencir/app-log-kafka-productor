e7329b69f5531593636b140e2162c72a2c1dc953
Mencir <smithpalomares4@gmail.com>
2025-08-17 01:57:19 -0500

    Inicializacion del productor kafka

---
+++ src/main/java/pe/eu/idat/app_log_kafka_productor/AppLogKafkaProductorApplication.java
@@ -0,0 +1,13 @@
+package pe.eu.idat.app_log_kafka_productor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLogKafkaProductorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLogKafkaProductorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/eu/idat/app_log_kafka_productor/AppLogKafkaProductorApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.eu.idat.app_log_kafka_productor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLogKafkaProductorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
